120d6899b4e4669d5c36f876cafc04405284955d
하나 <hana@hanaui-noteubug.local>
2025-02-08 02:13:00 +0900

    feat: 전진 여부 함수 추가

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -1,16 +1,23 @@
+import random
 def main():
     print("프로그램이 시작되었습니다.")
 
     car_names = input("경주할 자동차 이름을 입력하세요.(이름은 쉼표로 구분)\n")
     cars = car_names.split(",")
+    car_positions = {car : 0 for car in cars}
 
     print(cars)
+    print(car_positions)
 
-    attempts = input("시도할 횟수는 몇 회인가요?\n")
+    attempts = int(input("시도할 횟수는 몇 회인가요?\n"))
 
-    print("실행 결과\n")
+    print("\n실행 결과")
 
+        
+def movement(cars, car_positions) :
+    for car in cars :
+        if (random.randint(0,9) >= 4) :
+            car_positions[car] += 1
 
 if __name__ == "__main__":
-    # 프로그램이 직접 실행될 때만 main() 함수를 호출
     main()
