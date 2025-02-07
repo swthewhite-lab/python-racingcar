f1efa11201ccfded317b1bb06d9f90a6eb1134dd
하나 <hana@hanaui-noteubug.local>
2025-02-08 02:25:40 +0900

    feat: 최종 우승자 출력 함수 추가

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -6,18 +6,31 @@ def main():
     cars = car_names.split(",")
     car_positions = {car : 0 for car in cars}
 
-    print(cars)
-    print(car_positions)
-
     attempts = int(input("시도할 횟수는 몇 회인가요?\n"))
 
     print("\n실행 결과")
+    for i in range(attempts) :
+        movement(cars, car_positions)
+        for car in cars :
+            print(car + " : " + '-' * car_positions[car])
+        print()
 
+    printWinner(cars, car_positions)
         
 def movement(cars, car_positions) :
     for car in cars :
         if (random.randint(0,9) >= 4) :
             car_positions[car] += 1
+
+def printWinner(cars, car_positions) :
+    winners = []
+    max_value = max(car_positions.values())
+
+    for car in cars : 
+        if (car_positions[car] == max_value) :
+            winners.append(car)
+    
+    print(f"최종 우승자 : {', '.join(winners)}")
 
 if __name__ == "__main__":
     main()
