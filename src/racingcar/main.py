0a0f230ea00bde5298738a1917f8bdf6accb067e
하나 <hana@hanaui-noteubug.local>
2025-02-08 01:52:47 +0900

    feat: 기본 입출력 기능

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -1,10 +1,14 @@
 def main():
-    """
-    프로그램의 진입점 함수.
-    여기에서 전체 프로그램 로직을 시작합니다.
-    """
-    # 프로그램의 메인 로직을 여기에 구현
     print("프로그램이 시작되었습니다.")
+
+    car_names = input("경주할 자동차 이름을 입력하세요.(이름은 쉼표로 구분)\n")
+    cars = car_names.split(",")
+
+    print(cars)
+
+    attempts = input("시도할 횟수는 몇 회인가요?\n")
+
+    print("실행 결과\n")
 
 
 if __name__ == "__main__":
