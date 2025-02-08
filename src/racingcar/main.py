844cbd080b3c5fa755b531489b8eda88fdab1df8
chaerishme <scw010814@gmail.com>
2025-02-08 23:50:54 +0900

    style: 독스트링 추가

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -2,6 +2,11 @@ import random
 
 
 def main():
+    """
+    프로그램을 실행하고, 자동차 경주의 시뮬레이션을 수행하는 함수
+    사용자로부터 자동차 이름과 시도 횟수를 입력받고, 각 시도마다
+    경주를 진행하여 최종 우승자를 출력.
+    """
     print("프로그램이 시작되었습니다.")
 
     car_names = input("경주할 자동차 이름을 입력하세요.(이름은 쉼표로 구분)\n")
@@ -27,11 +32,18 @@ def main():
     print_winner(cars, car_positions)
 
 def movement(cars, car_positions):
+    """
+    각 자동차가 0에서 9 사이의 랜덤 숫자를 생성하여, 
+    4 이상의 숫자가 나오면 해당 자동차의 위치를 1 증가시키는 함수
+    """
     for car in cars :
         if random.randint(0,9) >= 4 :
             car_positions[car] += 1
 
 def get_winner(cars, car_positions):
+    """
+    경주에서 우승한 자동차를 구하는 함수입니다.
+    """
     max_value = max(car_positions.values())
     return [car for car in cars if car_positions[car] == max_value]
 
