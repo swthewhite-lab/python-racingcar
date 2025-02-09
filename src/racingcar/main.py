239d2234fa2069b3084a9197d2f5a827e9aae3b0
akran <nhlk123@naver.com>
2025-02-09 13:23:23 +0900

    feat: 자동차 전진 기능 구현

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -1,16 +1,32 @@
-def main():
-    def check_carname(x):
+import random
+def check_carname(x):
         for i in x :
             if len(i)>5:
                 raise ValueError
-    
-    
+
+def carfoward():
+    a=random.randint(0,9)
+    if a>=4 :
+        return "-"
+    else :
+        return ""
+
+def main():
     car_name=input("경주할 자동차 이름을 입력하세요.(이름은 쉼표로 구분)").split(',')
     check_carname(car_name)
     
     Cars=dict.fromkeys(car_name,"")
+    
+    #trycount=int(input())
+    
+    for i in car_name:
+        Cars[i]+=carfoward()
+
+    for i in car_name:
+        Cars[i]+=carfoward()
 
     print(Cars)
+
 
 
     
