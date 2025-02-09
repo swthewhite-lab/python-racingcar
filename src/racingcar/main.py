7df91b9bf17d493dd0712e68982b2aa7a3ebc8bc
akran <nhlk123@naver.com>
2025-02-09 12:22:35 +0900

    feat: 자동차 이름입력 기능

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -1,9 +1,12 @@
 def main():
-    """
-    프로그램의 진입점 함수.
-    여기에서 전체 프로그램 로직을 시작합니다.
-    """
-    # 프로그램의 메인 로직을 여기에 구현
+
+    Cars=[]
+    
+    racing_carInput=input().split()
+    print(racing_carInput)
+
+
+    
     print("프로그램이 시작되었습니다.")
 
 
