0b7e82a63026a80427bd312156c353865edbeac8
김성규 <kof9e1@naver.com>
2025-02-10 21:30:12 +0900

    이동 횟수 입력 기능 추가

--- src/racingcar/main.py
+++ src/racingcar/main.py
@@ -6,3 +6,12 @@ def get_car_names():
         raise ValueError("자동차 이름은 1~5자의 문자여야 합니다.")
 
     return names
+
+def get_attempt_count():
+    try:
+        count = int(input("시도할 횟수는 몇 회인가요?\n"))
+        if count <= 0:
+            raise ValueError
+        return count
+    except ValueError:
+        raise ValueError("올바른 횟수를 입력하세요. (양의 정수)")
